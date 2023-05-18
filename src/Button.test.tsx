0ad41bb7457221d4a5d9af8eed6f7846afafa585
evanvidel <evanvidel@gmail.com>
2023-05-18 14:44:05 -0300

    testing library basics

---
+++ src/Button.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen, fireEvent } from "@testing-library/react"
+import Button from "./Button"
+
+describe('Button Component', () => {
+    it('should render with red background if disabled', () => {
+        render(<Button disabled={false} onClick={() => { }}>Click me</Button>);
+
+        const button = screen.getByRole('button', { name: 'Click me' })
+        expect(button).toHaveStyle({ backgroundColor: "blue" });
+    });
+
+    it('should call onclick prop on click', () => {
+        const onClick = jest.fn();
+        render(<Button disabled onClick={onClick}>
+            Click me
+        </Button>
+    );
+
+    const button = screen.getByText(/click me/i);
+    fireEvent.click(button)
+
+    expect(onClick).toHaveBeenCalled();
+
+    });
+})
